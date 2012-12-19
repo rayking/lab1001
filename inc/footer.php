c256cbf7cb6685362ee6b57f4726e5a61ebeab14
rayking <raykingcn@msn.com>
2012-12-19 17:05:49 +0800

    cms搭建

--- inc/footer.php
+++ inc/footer.php
@@ -1,9 +1,34 @@
+<?php
+if($nocommont){
+
+?> 
+<div class="ujian-hook"></div>
+  <!-- UY BEGIN -->
+<div id="uyan_frame"></div>
+<script type="text/javascript" id="UYScript" src="http://v1.uyan.cc/js/iframe.js?UYUserId=1532596" async=""></script>
+<!-- UY END -->
+
+<!-- UJian Button BEGIN -->
+<script type="text/javascript">var ujian_config = {num:6,showType:3,bgColor:'#'};</script>
+<script type="text/javascript" src="http://v1.ujian.cc/code/ujian.js?uid=1532596"></script>
+<a href="http://www.ujian.cc" style="border:0;"><img src="http://img.ujian.cc/pixel.png" alt="友荐云推荐" style="border:0;padding:0;margin:0;" /></a>
+<!-- UJian Button END -->
+<?php
+}
+?>
  </article>
+
 </div>
 <footer class="container" id="footer">
 		
 		<p><small>&copy; Copyright rAy 2012. All Rights Reserved.</small></p>
 		
 	</footer>
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fdbd40babfe06b418a82db3f19051ee6b' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
+
 </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,13 @@
 <?php
 /*通用方法*/
-$SITEURL = 'http://192.168.1.9/lab1001/';
-$HASSUBFOLDER = true;
+
+	
+$isDev = $_SERVER['HTTP_HOST'] == 'localhost';
+$SITEURL = $isDev ? 'http://localhost/lab1001/' :'http://lab.baobao1001.com/';
+
 $pathinfo = explode("/",$_SERVER['REQUEST_URI']);
-if($HASSUBFOLDER){
+
+if($isDev){
 	unset($pathinfo[0]);
 	unset($pathinfo[1]);
 	$pathinfo = array_values($pathinfo);
@@ -16,6 +20,7 @@ $parentfoldername = $pathinfo[sizeof($pathinfo)-3];
 */
 $cate_name = $parentfoldername ? $parentfoldername :$foldername;
 $cate_actives = array($cate_name =>' class="active"');
+$nocommont = $filename == ('index.php' || '');
 /*print_r($pathinfo);
 */
 ?>
@@ -37,8 +42,8 @@ $cate_actives = array($cate_name =>' class="active"');
         g.firstChild);l.media="print";m.attachEvent("onbeforeprint",function(){var d=-1,a=p(c.styleSheets,"all"),e=[],b;for(f=f||c.body;(b=u.exec(a))!=null;)e.push((b[1]+b[2]+b[3]).replace(s,"$1.iepp_$2")+b[4]);for(l.styleSheet.cssText=e.join("\n");++d<o;){a=c.getElementsByTagName(i[d]);e=a.length;for(b=-1;++b<e;)if(a[b].className.indexOf("iepp_")<0)a[b].className+=" iepp_"+i[d]}r.appendChild(f);k.appendChild(h);h.className=f.className;h.innerHTML=f.innerHTML.replace(t,"<$1font")});m.attachEvent("onafterprint",
         function(){h.innerHTML="";k.removeChild(h);k.appendChild(f);l.styleSheet.cssText=""})}})(this,document);@*/
        var KillIE =(function(){
-          clipboardData.setData("text","http://lab.baobao1001.com/");
-          var leave = confirm("消灭低版本IE从我做起，请用现代浏览器浏览本站内容，网址已复制到粘贴板");
+        
+          var leave = confirm("消灭低版本IE从我做起，请用现代浏览器浏览本站内容");
        })();
     </script>
     
